a0b37f4b8480c8d56904c06692ecdc28f0b9376c
LastWolf <83525369+lastcrywolf@users.noreply.github.com>
2021-10-28 04:06:51 -0400

    Add files via upload

---
+++ Random_Password_Generator.py
@@ -0,0 +1,27 @@
+# Import relevant modules
+import random
+
+# All uppercase password
+password = ""
+for i in range(10):
+    i = chr(random.randint(65, 90))
+    password = str(password) + i
+print(password)
+
+# Upper and lower case password
+password = ""
+for i in range(5):
+    i = chr(random.randint(65, 90))
+    for j in range(5):
+        j = chr(random.randint(65,90)).lower()
+    password = str(password) + j + i
+print(password)
+
+# The upper and lowercase password can be simplified as follows:
+password = ""
+for i in range(5):
+    i = chr(random.randint(65, 90))
+    j = chr(random.randint(65, 90)).lower()
+    password = str(password) + i + j
+print(password)
+
